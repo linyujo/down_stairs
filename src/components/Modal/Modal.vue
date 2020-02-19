b0861168e07d20d3af33284e1bf7b0a7641c805f
raine <raine.lin@tapsystem.co>
2020-02-19 22:32:36 +0800

    [Feature]新增階梯說明Modal

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,109 @@
+<template>
+	<div className="modal">
+		<div class="modal-mask">
+			<div class="modal-wrapper">
+				<div class="modal-container">
+					<button class="closeCross btn" @click="$emit('close')">
+						<span></span>
+						<span></span>
+					</button>
+					<div class="modal-header">
+						<slot name="customHeader">
+							default header
+						</slot>
+					</div>
+					<div class="modal-body">
+						<slot name="customBody">
+							default body
+						</slot>
+					</div>
+				</div>
+			</div>
+		</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Modal",
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-mask{
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
+  transition: opacity 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-container{
+	background-color: #fefefe;
+	width: 100%;
+	// max-width: 624px;
+	min-height: 96px;
+	border-radius: 4px;
+	margin: 0 auto;
+	position: relative;
+}
+.modal-header{
+	border-bottom: 1px solid rgba(34,36,38,.15);
+	padding: 1rem 1rem;
+	font-weight: bold;
+}
+.modal-body{
+	padding: 30px;
+}
+
+.btn{
+	/* Remove First 移除ios input style */
+	appearance: none;
+	position: relative;
+	box-sizing: border-box;
+	display: inline-block;
+	margin: 0;
+	border: 0;
+	outline: 0;
+	text-decoration: none;
+	letter-spacing: 0;
+	/* 使用者互動 */
+	cursor: pointer;
+	user-select: none;
+	background: transparent;
+}
+
+.closeCross{
+ 	width: 55px;
+	height: 55px;
+	position: absolute;
+	right: 0;
+	transition: all 0.3s;
+	&:hover{
+		transform: rotate(180deg);
+	}
+}
+
+.closeCross span{
+    width: 16px;
+    height: 2px;
+    background-color: #484848;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+	&:first-child{
+		transform: translate(-50%,-50%) rotate(45deg);
+	}
+	&:last-child{
+		transform: translate(-50%,-50%) rotate(-45deg);
+	}
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,124 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <Playground />
+		<Modal v-if="showModal" @close="showModal = false">
+			<div slot="customHeader">階梯說明</div>
+			<div slot="customBody">
+				<div class="container">
+					<div class="row">
+						<li v-for="(stair, index) in firstTwoItems" v-bind:key="index">
+							<div class="imageBox">
+								<div class="imageBox__ratio">
+									<div class="image" :style="stair.src" />
+								</div>
+							</div>
+							<div class="title">{{ stair.type }}</div>
+							<div class="descript">{{ stair.descript }}</div>
+						</li>
+					</div>
+					<div class="row">
+						<li v-for="(stair, index) in leftItems" v-bind:key="index">
+							<div class="imageBox">
+								<div class="imageBox__ratio">
+									<div class="image" :style="stair.src" />
+								</div>
+							</div>
+							<div class="title">{{ stair.type }}</div>
+							<div class="descript">{{ stair.descript }}</div>
+						</li>
+					</div>
+				</div>
+			</div>
+		</Modal>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Playground from "@/components/DownStairs/CanvasPlayground.vue";
+import Modal from "@/components/Modal/Modal.vue";
+
+const stairTypes = [
+	{
+		src: `background: url(${require("@/assets/stairsImg/blade.png")})`,
+		type: "尖刺階梯",
+		descript: "傷害，血量 - 1"
+	},
+	{
+		src: `background: url(${require("@/assets/stairsImg/jump.png")})`,
+		type: "彈跳階梯",
+		descript: "向上彈跳",
+	},
+	{
+		src: `background: url(${require("@/assets/stairsImg/normal.png")})`,
+		type: "彈跳階梯",
+		descript: "補血，血量 + 1",
+	},
+	{
+		src: `background: url(${require("@/assets/stairsImg/slide.png")})`,
+		type: "滑動階梯",
+		descript: "輸送帶，向左/向右滑動",
+	},
+	{
+		src: `background: url(${require("@/assets/stairsImg/sand.png")})`,
+		type: "流沙階梯",
+		descript: "會緩慢穿過掉下",
+	},
+];
 
 export default {
-  name: "home",
   components: {
-    Playground
-  }
+		Playground,
+		Modal,
+  },
+  data() {
+	  return {
+			showModal: true,
+			firstTwoItems: stairTypes.slice(0, 2),
+			leftItems: stairTypes.slice(2, 5),
+	  }
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+.container{
+	width: 800px;
+	.row{
+		display: flex;
+		justify-content: space-around;
+	}
+	.row:first-child{
+		margin-bottom: 24px;
+	}
+}
+
+li{
+	display: inline-block;
+	list-style: none;
+	.title{
+		margin: 8px 0 4px 0;
+    font-weight: bold;
+	}
+}
+.imageBox{
+		width: 228px;
+    overflow: hidden;
+}
+.imageBox .imageBox__ratio{
+    width: 100%;
+    padding-top: 50%;
+    position: relative;
+}
+.imageBox .image{
+    /* 定位 */
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    /* 圖片 */
+    background-size: cover;
+    background-position: center;
+}
+</style>
