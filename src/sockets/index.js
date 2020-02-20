40b4b10ef4943e8909f222ed41e3bb669f6277e5
raine <raine.lin@tapsystem.co>
2020-02-20 20:37:36 +0800

    [Fixed]Fixed build error

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -5,18 +5,18 @@ const wsClient = new WebSocket(wsuri);
 
 // 發送
 wsClient.onopen = () => {
-	const msg = {
-		type: "message",
-		text: "wow",
-		id: "123",
-		date: Date.now(),
-	};
-	wsClient.send(JSON.stringify(msg));
+  const msg = {
+    type: "message",
+    text: "wow",
+    id: "123",
+    date: Date.now()
+  };
+  wsClient.send(JSON.stringify(msg));
 };
 
 // 接收
-wsClient.onmessage = function (event) {
-	console.log(event.data);
+wsClient.onmessage = function() {
+  // console.log(event.data);
 };
 
 export default wsClient;
